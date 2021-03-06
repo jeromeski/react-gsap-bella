4d2ad24b2950bbabfd7bfddd56343c1e537e1f0e
jeromeski <jeromegee@gmail.com>
2021-03-06 17:01:17 +0800

    Reset Image Position

--- src/components/Reveal/index.js
+++ src/components/Reveal/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import gsap from 'gsap/gsap-core';
+import React, { useEffect, useRef } from 'react';
 import { rgData } from '../../data';
 
 const Reveal = () => {
+	//  get elements for animation
+	let revealEl = useRef([]);
+	let imageBlockEl = useRef([]);
+	let maskEl = useRef([]);
+
+	const initHoverReveal = () => {
+		revealEl.current.forEach((section, idx) => {
+			// reset the initial position
+			gsap.set(imageBlockEl.current[idx], {
+				yPercent: -101
+			});
+			gsap.set(maskEl.current[idx], { yPercent: 100 });
+
+			section.addEventListener('mouseenter', createHoverReveal);
+			section.addEventListener('mouseleave', createHoverReveal);
+		});
+	};
+
+	const createHoverReveal = (e) => {
+		console.log(e.type);
+	};
+
+	useEffect(() => {
+		initHoverReveal();
+	});
+
 	return (
 		<article>
 			<section className='reveal-gallery'>
 				{rgData().map((data, idx) => (
-					<div key={idx} className={`rg__column ${data.classL1} is-active`} data-color={data.color}>
+					<div
+						key={idx}
+						className={`rg__column ${data.classL1} is-active`}
+						data-color={data.color}
+						ref={(el) => (revealEl.current[idx] = el)}>
 						<div className='rg__text'>
 							<div className='rg__text--heading'>
 								<span>{data.titleNum}</span>
@@ -18,8 +49,11 @@ const Reveal = () => {
 								</div>
 							</div>
 						</div>
-						<div className='rg__image'>
-							<div className='rg__image--mask' style={{ transform: data.style }}>
+						<div className='rg__image' ref={(el) => (imageBlockEl.current[idx] = el)}>
+							<div
+								className='rg__image--mask'
+								style={{ transform: data.style }}
+								ref={(el) => (maskEl.current[idx] = el)}>
 								<img
 									src={require(`../../assets/img/${data.imgName}.jpg`).default}
 									alt={data.altName}
